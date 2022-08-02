962238b0461f968d5558df64ce5557ceac3d61f6
SHamidGh <seyyedhamid.ghasemi@gmail.com>
2022-08-02 10:55:19 +0430

    tarrahi

--- src/main/java/Controllers/MainPageController.java
+++ src/main/java/Controllers/MainPageController.java
@@ -1,13 +1,17 @@
 package Controllers;
 
 import View.Controller;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
+import java.io.IOException;
+
 public class MainPageController {
+
     @FXML
     private HBox LogOutButton;
 
@@ -15,12 +19,52 @@ public class MainPageController {
     private GridPane main;
 
     @FXML
-    public void initialize(){
-        LogOutButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                Controller.stage.setScene(Controller.startPage);
-            }
-        });
+    void AnalysePage(MouseEvent event) {
+
     }
+
+    @FXML
+    void Exit(MouseEvent event) {
+        Controller.stage.setScene(Controller.startPage);
+    }
+    @FXML
+    void Explore(MouseEvent event) {
+
+    }
+    @FXML
+    void Followers(MouseEvent event) {
+
+    }
+    @FXML
+    void Followings(MouseEvent event) {
+
+    }
+    @FXML
+    void Group(MouseEvent event) {
+
+    }
+    @FXML
+    void Home(MouseEvent event) {
+
+    }
+    @FXML
+    void MainPage(MouseEvent event) {
+
+    }
+    @FXML
+    void Pv(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("/fxml/PvsPage.fxml"));
+        Scene scene=fxmlLoader.load();
+        PvPageController pvPageController=fxmlLoader.getController();
+        Controller.stage.setScene(scene);
+    }
+    @FXML
+    void SaveMessage(MouseEvent event) {
+
+    }
+    @FXML
+    void Setting(MouseEvent event) {
+
+    }
+
 }
---
+++ src/main/java/ShowClass/MyMessageController.java
@@ -0,0 +1,4 @@
+package ShowClass;
+
+public class MyMessageController {
+}
